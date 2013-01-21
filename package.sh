18f175d4fed6a1082cdfefa46fcec7dfca905af1
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2013-01-21 20:32:01 +0000

    Versioning and better naming of downloadables.

--- package.sh
+++ package.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-java -jar closure-compiler/compiler.jar  --compilation_level SIMPLE_OPTIMIZATIONS --js src/KeyBindings.js --js src/TableFilterer.js --js src/jquery.inc-6.js --js src/CsvExtractor.js --js src/InlineCellEditor.js --js src/TableLoader.js --js src/TableEditor.js --js src/Constructor.js --js_output_file download-files/csvera.js
+version=$(cat minor_version.txt)
+#let version++
+#echo $version > minor_version.txt
 
-cp src/csvera.css src/jquery-1.7.1.min.js src/underscore-min.js download-files
+target="download/files"
+minifiedName="csvera-1.${version}-min.js"
 
-zip -rv -x *.zip @ download-all.zip download-files/*
+cat ${target}/CSVera.template | sed "s/@CSVERA@/${minifiedName}/" > ${target}/CSVera.html
+
+java -jar closure-compiler/compiler.jar  --compilation_level SIMPLE_OPTIMIZATIONS --js src/KeyBindings.js --js src/TableFilterer.js --js src/jquery.inc-6.js --js src/CsvExtractor.js --js src/InlineCellEditor.js --js src/TableLoader.js --js src/TableEditor.js --js src/Constructor.js --js_output_file ${target}/${minifiedName}
+
+cp src/csvera.css src/jquery-1.7.1.min.js src/underscore-min.js ${target}
+
+zip -rv -x *.zip @ download/csvera-1.${version}.zip ${target}/*
