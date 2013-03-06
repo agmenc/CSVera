efb972bc3f4698ef27efe82e4d9f85dda0b04aee
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2013-03-06 17:35:43 +0000

    Make URL updating work. Update list.

--- src/QueryParam.js
+++ src/QueryParam.js
@@ -1,22 +1,24 @@
+var debug;
+
 function param(name, value) {
     if (typeof value === "undefined") return allParams()[name];
     else setParam(name, value);
 
     function setParam(name, value) {
-        console.log("Updating param: " + name + " = " + value);
         var params = allParams();
         params[name] = value;
 
-        // TODO - CAS - 21/02/2013 - Use document.location.search, to get the "?" and everything afterwards
-        var baseUrl = document.location.href.split(/\?|&/)[0];
-        var acc = "";
-        for (var i in params) acc = acc + i + "=" + params[i] + "&";
-
-        // How can we change the URL without reloading the page?
-        // Infinite loop:
-//        document.location.href = baseUrl + "?" + acc;
+        var acc = [];
+        for (var k in params) if (params.hasOwnProperty(k)) acc.push(parameter(k, params));
+        history.pushState({ path: this.path }, '', "?" + acc.join("&"))
     }
 
+    function parameter(key, params) {
+//        if (typeof params[key] === "undefined") return "";
+        return key + "=" + params[key];
+    }
+
+    // TODO - CAS - 21/02/2013 - Use document.location.search, to get the "?" and everything afterwards
     function allParams() {
         var z = {};
         var x = document.location.href.split(/\?|&/).splice(1);
