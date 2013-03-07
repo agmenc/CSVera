bb796b95f5386e478fd0ea40acb36fcc7bce63bf
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2013-03-07 08:36:12 +0000

    Tidy up param(k, v)

--- src/QueryParam.js
+++ src/QueryParam.js
@@ -1,24 +1,17 @@
-var debug;
-
 function param(name, value) {
-    if (typeof value === "undefined") return allParams()[name];
-    else setParam(name, value);
+    if (typeof value === "undefined") return allParams()[name.trim()];
+    else setParam(name.trim(), value);
 
     function setParam(name, value) {
         var params = allParams();
         params[name] = value;
 
         var acc = [];
-        for (var k in params) if (params.hasOwnProperty(k)) acc.push(parameter(k, params));
+        for (var k in params) if (params.hasOwnProperty(k)) acc.push(k + "=" + params[k]);
         history.pushState({ path: this.path }, '', "?" + acc.join("&"))
     }
 
-    function parameter(key, params) {
-//        if (typeof params[key] === "undefined") return "";
-        return key + "=" + params[key];
-    }
-
-    // TODO - CAS - 21/02/2013 - Use document.location.search, to get the "?" and everything afterwards
+    // TODO - CAS - 21/02/2013 - Use document.location.search, to get the "?" and everything afterwards - test in FireFox
     function allParams() {
         var z = {};
         var x = document.location.href.split(/\?|&/).splice(1);
